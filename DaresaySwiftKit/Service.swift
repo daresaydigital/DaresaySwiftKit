59e48ff0e1d51ef65f2044b90dad4486602977c0
Arslan Rafique <mailtoarslan@gmail.com>
2019-09-24 13:29:15 +0200

    Add initial files

---
+++ DaresaySwiftKit/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  DaresaySwiftKit
+//
+//  Created by Arslan Rafique on 2019-09-24.
+//  Copyright © 2019 Daresay AB. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() { }
+    
+    public static func saySomething() -> String {
+        return "Something said..."
+    }
+}
