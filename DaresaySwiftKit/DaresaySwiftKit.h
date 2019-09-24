f76e3c2a38e3abbe54534eea3f5461ee765348d2
Arslan Rafique <mailtoarslan@gmail.com>
2019-09-24 13:20:32 +0200

    Initial Commit

---
+++ DaresaySwiftKit/DaresaySwiftKit.h
@@ -0,0 +1,19 @@
+//
+//  DaresaySwiftKit.h
+//  DaresaySwiftKit
+//
+//  Created by Arslan Rafique on 2019-09-24.
+//  Copyright © 2019 Daresay AB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DaresaySwiftKit.
+FOUNDATION_EXPORT double DaresaySwiftKitVersionNumber;
+
+//! Project version string for DaresaySwiftKit.
+FOUNDATION_EXPORT const unsigned char DaresaySwiftKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DaresaySwiftKit/PublicHeader.h>
+
+
